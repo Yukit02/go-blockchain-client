eb914e45a9be96fa6c291c517cfac6fe3b50b3e5
Yukit02 <yukijobit02@gmail.com>
2022-03-10 11:14:02 +0900

    [add] install tailwind css

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import { Quasar } from 'quasar';
 import App from './App.vue';
 import quasarUserOptions from './quasar-user-options.js';
+import './styles/tailwind.css';
 
 createApp(App).use(Quasar, quasarUserOptions).mount('#app');
